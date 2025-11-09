256b5639653dbbb7f5eb17350a94b448f4d56b17
Dhruv <dhruvrai28069@gmail.com>
2025-11-10 01:52:11 +0530

    Java IntToString

---
+++ IntToString.java
@@ -0,0 +1,15 @@
+import java.io.*;
+        import java.util.*;
+
+public class IntToString {
+
+    static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        String s = String.valueOf(n);
+        System.out.println("Good job");
+
+    }
+}
