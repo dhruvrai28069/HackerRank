ccddc477bda75c215b832b6913cdfdbf298f2c4e
Dhruv <dhruvrai28069@gmail.com>
2025-11-10 01:51:27 +0530

    Java EndOfLine

---
+++ EndOfLine.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class EndOfLine {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int lineNumber = 1;
+
+        while(sc.hasNextLine()){
+
+            String line = sc.nextLine();
+            System.out.println( lineNumber + " " + line);
+
+            lineNumber ++;
+        }
+    }
+}
