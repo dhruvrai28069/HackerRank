f59a4f2cc6d96d66b8865c0dd298ddf58e1c7865
Dhruv <dhruvrai28069@gmail.com>
2025-11-10 01:51:52 +0530

    Java StaticInitializerBlock

---
+++ StaticInitializerBlock.java
@@ -0,0 +1,20 @@
+import java.io.*;
+        import java.util.*;
+
+public class StaticInitializerBlock {
+
+     static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        if(a <= 0 || b <= 0){
+            System.out.println("java.lang.Exception: Breadth and height must be positive");
+        }
+        else{
+            System.out.println(a * b);
+        }
+    }
+}
