ea74c98ef7a80e34e5e2f27c0d23219fb3a01743
Dhruv <dhruvrai28069@gmail.com>
2025-12-15 23:51:26 +0530

    Java String Reverse

---
+++ JavaStringReverse.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringReverse {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        String A = sc.next();
+
+        int head = 0;
+        int tail = A.length() - 1;
+        boolean isPalindrome = true;
+
+        while (head < tail) {
+            if (A.charAt(head) != A.charAt(tail)) {
+                isPalindrome = false;
+                break;
+            }
+            head++;
+            tail--;
+        }
+
+        if (isPalindrome) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
